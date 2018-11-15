c933fc92379c0b2d39ad70d2851a4d6506acb766
TeelaBrown <45015269+TeelaBrown@users.noreply.github.com>
2018-11-15 12:22:48 +0000

    fix

--- pimpit.sh
+++ pimpit.sh
@@ -5,7 +5,6 @@ rm -r /etc/update-motd.d/
 mkdir /etc/update-motd.d/
 touch /etc/update-motd.d/00-header ; touch /etc/update-motd.d/10-sysinfo ; touch /etc/update-motd.d/90-footer
 chmod +x /etc/update-motd.d/*
-rm /etc/motd.dynamic
 cat 00-header > /etc/update-motd.d/00-header
 cat 10-sysinfo > /etc/update-motd.d/10-sysinfo
 cat 90-footer > /etc/update-motd.d/90-footer
