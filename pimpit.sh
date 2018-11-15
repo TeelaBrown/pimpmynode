8da78caa47cb87a775eae625b6cf8240bc8782a2
TeelaBrown <45015269+TeelaBrown@users.noreply.github.com>
2018-11-15 12:19:49 +0000

    remove unused package, fix

--- pimpit.sh
+++ pimpit.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 apt-get update -y
-apt-get install lsb-release figlet update-motd -y
+apt-get install lsb-release update-motd -y
 rm -r /etc/update-motd.d/
 mkdir /etc/update-motd.d/
 touch /etc/update-motd.d/00-header ; touch /etc/update-motd.d/10-sysinfo ; touch /etc/update-motd.d/90-footer
 chmod +x /etc/update-motd.d/*
+rm /etc/motd.dynamic
 cat 00-header > /etc/update-motd.d/00-header
 cat 10-sysinfo > /etc/update-motd.d/10-sysinfo
 cat 90-footer > /etc/update-motd.d/90-footer
